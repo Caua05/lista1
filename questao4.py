19c5359de959a054ef774e684129ffa67c76c422
Caua05 <162156499+Caua05@users.noreply.github.com>
2024-05-03 14:26:39 -0300

    Add files via upload

---
+++ questao4.py
@@ -0,0 +1,5 @@
+def convite_festa():
+    nome = input("digite seu nome: ")
+    nome_em_caixa_alta = nome.upper()
+    print("voce esta convidado(a) para a festa," + nome_em_caixa_alta)
+convite_festa()
